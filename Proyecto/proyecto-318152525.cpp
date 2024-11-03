c4221d2bacb251f77ad550d2fb6631ce4d54db49
Jose Luis Rivera <riveraluis6765@gmail.com>
2024-11-02 18:40:57 -0600

    recorrido correcto

--- Proyecto/proyecto-318152525.cpp
+++ Proyecto/proyecto-318152525.cpp
@@ -2610,7 +2610,7 @@ int main()
 		//Stewie
 		model = glm::mat4(1.0);
 		model = glm::translate(model, glm::vec3(-125.0f + movPerX, 6.0f, 130.0f + movPerZ));
-		model = glm::rotate(model,(90 *toRadians)+(rotPer * toRadians), glm::vec3(0.0f, 1.0f, 0.0f));
+		model = glm::rotate(model,(90 *toRadians)+(-rotPer * toRadians), glm::vec3(0.0f, 1.0f, 0.0f));
 		model = glm::scale(model, glm::vec3(10.0f, 10.0f, 10.0f));
 		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
 		Stewie_M.RenderModel();
